38edf183a0733c62a3b79b4f915f1859ac029e18
Justin Strelka <jstrelka@student.cccs.edu>
2019-10-17 11:58:32 -0600

    made provider table changes to ipps.sql

--- ipps.sql
+++ ipps.sql
@@ -7,10 +7,17 @@ CREATE DATABASE ipps;
 USE ipps;
 
 --We need to 3rd normalize the 12 csv columns and create tables
-CREATE TABLE DataStuff (
- --   id INT(3) NOT NULL PRIMARY KEY, 
- --   name VARCHAR(35) NOT NULL, 
- --   sal INT(6) );
+CREATE TABLE Providers (
+    providerID INT NOT NULL PRIMARY KEY, 
+    providerName VARCHAR(100) NOT NULL, 
+    providerStreet VARCHAR(120),
+    providerCity VARCHAR(100),
+    providerState CHAR(2),
+    providerZip CHAR(5),
+    referralState CHAR(2),
+    referralCity VARCHAR(100),
+    totalDischarges INT
+     );
 
 CREATE TABLE After3rdNormal (
  --   id INT(3) NOT NULL PRIMARY KEY, 
