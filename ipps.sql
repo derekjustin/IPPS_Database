af28f9a88834d7264b00285a677aca93faa14f28
Justin Strelka <jstrelka@student.cccs.edu>
2019-10-22 10:30:47 -0600

    removed raw_df tables

--- ipps.sql
+++ ipps.sql
@@ -8,24 +8,6 @@ CREATE DATABASE IF NOT EXISTS ipps;
 -- SELECT DATABASE TO OPERATE ON
 USE ipps;
 
--- TODO: REMOVE raw_df table as it is only for reference
-CREATE TABLE IF NOT EXISTS raw_df (
-    providerID INT NOT NULL, 
-    providerName VARCHAR(100) NOT NULL, 
-    providerStreetAddress VARCHAR(120) NOT NULL,
-    providerCity VARCHAR(100) NOT NULL,
-    providerState CHAR(2) NOT NULL,
-    providerZipCode CHAR(5) NOT NULL,
-    totalDischarges INT NOT NULL,
-    averageCoveredCharges DECIMAL(20,3) NOT NULL,
-    averageTotalPayments DECIMAL(20,3) NOT NULL,
-    averageMedicarePayments DECIMAL(20,3) NOT NULL,
-    dRgKey INT NOT NULL,
-    dRgDescription VARCHAR(120) NOT NULL,
-    referralRegionState CHAR(5) NOT NULL,
-    referralRegionDescription VARCHAR(120) NOT NULL
-    );
-
 -- CREATE providers TABLE
 CREATE TABLE IF NOT EXISTS providers (
     providerID INT PRIMARY KEY NOT NULL, 
